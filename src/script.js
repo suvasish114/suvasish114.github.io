f629d33704cba75bff771f3e0758dc8ea478cdf0
suvasish114 <misuvasish114@gmail.com>
2023-02-18 08:44:14 +0530

    version 4.0

--- src/script.js
+++ src/script.js
@@ -1,13 +1,14 @@
 let expand = document.getElementById("expand");
 let expandbtn = document.getElementById("expandbtn");
+navbar = document.getElementById("navbar");
 function toggle() {
     if (window.screen.width <= 768 && !expand.classList.contains("collapsed")) {
-        document.getElementById("navbar").style.boxShadow = "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px";
+        // navbar.style.boxShadow = "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px";
         expandbtn.classList.remove("bi-list");
         expandbtn.classList.add("bi-x-lg");
     }
     else {
-        document.getElementById("navbar").style.boxShadow = "none";
+        navbar.style.boxShadow = "none";
         expandbtn.classList.add("bi-list");
         expandbtn.classList.remove("bi-x-lg");
     }
